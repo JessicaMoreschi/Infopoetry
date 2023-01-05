09e47459da795e4337a99e8a7f455e983fecd155
JessicaMoreschi <71377205+JessicaMoreschi@users.noreply.github.com>
2023-01-05 11:52:30 +0100

    okk-before d3

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-let slideValue = 0;
-
+// TOGGLE INFO PANEL |––––––––––––––––––––––––––––––––––––––––––
 function openHelp() {
     let w = document.getElementById('main-container')
     if (w.style.width == '100%') {
         w.style.width = 'calc(100% - 500px)'
     } else { w.style.width = '100%' }
 }
+
+// SCROLLING TEXT |––––––––––––––––––––––––––––––––––––––––––
+let leftValue = 0
+function text(msg, ctrlwidth) {
+    msg = "  ●  " + msg
+    newmsg = msg
+    while (newmsg.length < ctrlwidth) {
+        newmsg += msg
+    }
+    document.write('<FORM NAME="Scrolltext">');
+    document.write('<INPUT id="runningTxt" NAME="text" VALUE= "' + newmsg + '" SIZE= ' + ctrlwidth + ' disabled>');
+    document.write('</FORM>');
+    rollmsg()
+}
+function rollmsg() {
+    leftValue += 5
+    document.getElementById('runningTxt').scrollTo({
+        top: 0,
+        left: leftValue,
+        behavior: 'smooth'
+    });
+    bannerID = setTimeout("rollmsg()", 100)
+}
+
+msg = "Studying at university"
+ctrlwidth = innerWidth
+text(msg, ctrlwidth);
